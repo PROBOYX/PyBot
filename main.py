af9ac4b98b55f4e41adfbc54a08dbddd92ce1eeb
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2023-02-20 17:23:21 +0530

    lets see

--- main.py
+++ main.py
@@ -1,57 +1,89 @@
+# COPYRIGHT 2023 BY ULTRA X
 from telethon  import TelegramClient, events, Button
+import asyncio
+import io
+import os
+import sys
+import traceback
 
-CODE = {'A': '.-',     'B': '-...',   'C': '-.-.', 
-        'D': '-..',    'E': '.',      'F': '..-.',
-        'G': '--.',    'H': '....',   'I': '..',
-        'J': '.---',   'K': '-.-',    'L': '.-..',
-        'M': '--',     'N': '-.',     'O': '---',
-        'P': '.--.',   'Q': '--.-',   'R': '.-.',
-        'S': '...',    'T': '-',      'U': '..-',
-        'V': '...-',   'W': '.--',    'X': '-..-',
-        'Y': '-.--',   'Z': '--..',   ' ': '/',
-
-        '0': '-----',  '1': '.----',  '2': '..---',
-        '3': '...--',  '4': '....-',  '5': '.....',
-        '6': '-....',  '7': '--...',  '8': '---..',
-        '9': '----.' 
-        }
-
-CODE_REVERSED = {value:key for key,value in CODE.items()}
-
-def encode(s):
-    return ' '.join(CODE.get(i.upper()) for i in s)
-
-def decode(s):
-    return ''.join(CODE_REVERSED.get(i) for i in s.split())
-
-
-
-def text(text):
-    if text.startswith(".") or text.startswith("-"):
-        decodeText = decode(text)
-        return decodeText
-    else:
-        encodeText = encode(text)
-        return encodeText
 
 api_id = 1621727
 api_hash = "31350903c528876f79527398c09660ce"
-token = "5627628015:AAHaCVsQ9o_cEhhdF1EayNCq2A-xXIX4Wzk"
+token = "5325238526:AAEcWx21slJatAOlPj-QuSWcNr-C58-vQYU"
 
 bot = TelegramClient("morse", api_id, api_hash).start(bot_token=token)
-
+xbot = bot
 @bot.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.respond("Forward/Send the text")
+    await event.respond("Yoo Im ready")
 
-@bot.on(events.NewMessage(incoming=True))
-async def translate(event):
-    if event.text == "/start":
-        pass
+
+
+
+@xbot.on(events.NewMessage(pattern="/eval ?(.*)"))
+async def _(event):
+    cmd = event.text.split(" ", maxsplit=1)[1]
+    if not cmd:
+        return await event.reply("What should I run ?..\n\nGive me something to run, u dumbo!!")
+    proevent = await event.reply("Running.....")
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
     else:
-        Text = text(event.text)
-        print (Text)
-        await event.reply(str(Text))
+        evaluation = "Sᴜᴄᴄᴇss ✅"
+    final_output = f"**•  Eᴠᴀʟ : **\n`{cmd}` \n\n**•  Rᴇsᴜʟᴛ : **\n`{evaluation}` \n"
+    await proevent.edit(final_output)
+
+
+async def aexec(code, smessatatus):
+    message = event = smessatatus
+    p = lambda _x: print(_format.yaml_format(_x))
+    reply = await event.get_reply_message()
+    exec(
+        f"async def __aexec(message, event , reply, client, p, chat): "
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+    return await locals()["__aexec"](
+        message, event, reply, message.client, p, message.chat_id
+    )
+
+
+
+@xbot.on(events.NewMessage(pattern="/exec ?(.*)"))
+async def _(event):
+    cmd = event.text.split(" ", maxsplit=1)[1]
+    if not cmd:
+        return await event.reply("What should I execute?..\n\nGive me somwthing to execute, u dumbo!!")
+    proevent = await event.reply("Executing.....")
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
+    curruser = "UltraX"
+    uid = os.geteuid()
+    if uid == 0:
+        cresult = f"`{curruser}:~#` `{cmd}`\n`{result}`"
+    else:
+        cresult = f"`{curruser}:~$` `{cmd}`\n`{result}`"
+    await proevent.edit(cresult)
 
 
 bot.run_until_disconnected()
